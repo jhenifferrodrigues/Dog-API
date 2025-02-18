db8b0ff4bb8c48fed625a638fef70aa6c9ed537e
JHENIFFER DE SOUZA RODRIGUES BRANDÃO <24122390@CFP127ALUNO.LOCAL>
2025-02-18 10:53:33 -0300

    css feito

--- dogapi.js
+++ dogapi.js
@@ -1,32 +1,32 @@
-'use trict'
+'use strict';
 
-async function pesquisarFotos(raca){
-    const url = `https://dog.ceo/api/breed/$(raca)/images`
-    const response = await fetch(url)
-    const data = await response.json()
+async function pesquisarFotos(raca) {
+    const url = `https://dog.ceo/api/breed/${raca}/images`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.message;
+}
+
+function criarImagem(link) {
+    const galeria = document.getElementById('galeria');
+    const novaImg = document.createElement('img');
+    novaImg.src = link;
+    novaImg.alt = 'Imagem de cachorro';
+    novaImg.style.width = '200px';
+    novaImg.style.margin = '5px';
     
-    return data.message
+    galeria.appendChild(novaImg);
 }
 
-function criarImagem (link){
-    const galeria = document.getElementById('galeria')
-    const novaImg = document.getElementById('Img')
-    novaImg.src = link
-    
-    galeria.appendChild(novaImg)
+async function preencherFotos() {
+    const raca = document.getElementById('raca').value;
+    const fotos = await pesquisarFotos(raca);
+    const galeria = document.getElementById('galeria');
 
+    galeria.replaceChildren();
+    fotos.forEach(criarImagem);
+    console.log(fotos);
 }
 
-async function preencherFotos(){
-    const raca = document.getElementById('raca').value
-    const fotos = await pesquisarFotos(raca)
-    const galeria = document.getElementById('galeria')
-
-    galeria.repleceChildren('')
-    fotos.forEach (criarImagem)
-    console.log (fotos)
-
-}
-
-document.getElementById('Pesquisar')
-    .addEventListener('Click', preencherFotos)
+document.getElementById('pesquisar')
+    .addEventListener('click', preencherFotos);
